e9256ad453372f385f84ff0fcb239cd4abb4a0e5
sana Habil <sansoon@gmail.com>
2022-08-02 10:54:55 -0700

    first commit

---
+++ Predict_Destructuring.js
@@ -0,0 +1,76 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// Predection:
+/*
+Macs-MacBook-Pro:JavaScrip-FullStack sana$ nodemon Predict_Destructuring.js 
+[nodemon] 2.0.19
+[nodemon] to restart at any time, enter `rs`
+[nodemon] watching path(s): *.*
+[nodemon] watching extensions: js,mjs,json
+[nodemon] starting `node Predict_Destructuring.js`
+Tesla
+Mercedes
+[nodemon] clean exit - waiting for changes before restart
+
+*/
+
+
+ const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+// Predection: Error name is not defined : console.log(name);
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//Predection: [nodemon] starting `node Predict_Destructuring.js` 12345 undefined
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//Predection: [nodemon] starting `node Predict_Destructuring.js` false  true
+
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+/* Predection:
+value
+[ 1, 5, 1, 8, 3, 3 ]
+1
+5
+*/
